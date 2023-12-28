b482ab4892369382162204126c7f2af80ddbffbe
DoLap <71600006+blackbeans-1301@users.noreply.github.com>
2023-12-28 14:57:16 +0000

    full screen

--- packages/js/read/reading-helper.js
+++ packages/js/read/reading-helper.js
@@ -45,6 +45,8 @@ const onExpandReadingSpace = () => {
 
   const tableOfContents = document.getElementById("book-table-of-content");
   tableOfContents.classList.toggle("hidden");
+
+  toggleFullScreen(document.body);
 };
 
 const onScrollToTop = () => {
@@ -93,6 +95,38 @@ const resetColor = () => {
   backgroundColorInput.value = "#ffffff";
   textColorInput.value = "#000000";
 };
+
+function toggleFullScreen(elem) {
+  // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+  if (
+    (document.fullScreenElement !== undefined &&
+      document.fullScreenElement === null) ||
+    (document.msFullscreenElement !== undefined &&
+      document.msFullscreenElement === null) ||
+    (document.mozFullScreen !== undefined && !document.mozFullScreen) ||
+    (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)
+  ) {
+    if (elem.requestFullScreen) {
+      elem.requestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullScreen) {
+      elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+}
 
 window.addEventListener("scroll", function () {
   var banner = document.getElementById("banner");
